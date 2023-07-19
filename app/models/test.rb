eca271885e5807610a2ef4ee7cf2a59b8392de8b
hiepdd <dao.duc.hiep@sun-asterisk.com>
2023-07-19 16:07:08 +0700

    signup user

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   belongs_to :subject
   belongs_to :user
-  has_many :test_questions
+  has_many :test_questions, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
   end
 end

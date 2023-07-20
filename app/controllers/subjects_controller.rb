fe88f04baf7b8e4e6900f634497ad0c858f4fc4b
bach-long <nguyenbach19122002@gmail.com>
2023-07-20 13:45:14 +0700

    create subject_show page

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,20 @@
 class SubjectsController < ApplicationController
+  before_action :load_subject, except: :index
+
   def index
     @pagy, @subjects = pagy(Subject.newest,
                             items: Settings.pagination.per_page_10)
+  end
+
+  def show; end
+
+  private
+
+  def load_subject
+    @subject = Subject.find_by id: params[:id]
+    return if @subject
+
+    flash[:danger] = t "subjects.show.not_found"
+    redirect_to subjects_path
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :subject
+  belongs_to :creator, class_name: User.name, foreign_key: :user_id
   has_many :answers, dependent: :destroy
   has_many :test_questions, dependent: :destroy
 end

690ec41852ac62d0376483f2bdc357ecc256e85f
hiepdd <dao.duc.hiep@sun-asterisk.com>
2023-07-20 09:46:56 +0700

    login/logout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    return if logged_in?
+
     flash.now[:info] = t ".login_message" unless flash.keys.include? "info"
     @user = User.new
     render "users/new"

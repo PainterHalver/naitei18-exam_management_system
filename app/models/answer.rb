ec4ebd620f7d13365048c0361ed138cf81cb10e4
bach-long <nguyenbach19122002@gmail.com>
2023-07-24 08:08:31 +0700

    show_test

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,11 @@
 class Answer < ApplicationRecord
   belongs_to :question
   has_many :detail_answers, dependent: :destroy
+  has_many :test_questions, through: :detail_answers
 
   validates :content, presence: true
+
+  def set_disabled
+    true
+  end
 end
--- app/models/test_question.rb
+++ app/models/test_question.rb
@@ -2,5 +2,10 @@ class TestQuestion < ApplicationRecord
   belongs_to :question
   belongs_to :test
   has_many :detail_answers, dependent: :destroy
-  scope :order_asc_question_ids, ->{order :question_id}
+  scope :order_asc_question_ids, ->{order question_id: :asc}
+  has_many :answers, through: :detail_answers
+
+  def first_answer_id
+    answers.first&.id
+  end
 end

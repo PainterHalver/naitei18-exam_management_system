f3695fc1a9f9d37ac2823aed133ef4a1c4c88f90
hiepdd <dao.duc.hiep@sun-asterisk.com>
2023-08-16 16:23:57 +0700

    fix swagger json format

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,8 +1,19 @@
 module API
   class Base < Grape::API
-    formatter :json, API::Formatters::SuccessFormatter
-    error_formatter :json, API::Formatters::ErrorFormatter
-
     mount API::V1::Base
+    add_swagger_documentation(
+      api_version: "v1",
+      hide_documentation_path: true,
+      mount_path: "/api/v1/swagger_doc",
+      schemes: %w(http https),
+      security_definitions: {
+        Bearer: {
+          type: "apiKey",
+          name: "Authorization",
+          in: "header"
+        }
+      },
+      security: [Bearer: []]
+    )
   end
 end
--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -3,17 +3,14 @@ require "grape-swagger"
 module API
   module V1
     class Base < Grape::API
+      formatter :json, API::Formatters::SuccessFormatter
+      error_formatter :json, API::Formatters::ErrorFormatter
       mount API::V1::HealthCheck
       mount API::V1::Subjects
       mount API::V1::Tests
       mount API::V1::Questions
       mount API::V1::Auth
       mount API::V1::Users
-      add_swagger_documentation(
-        api_version: "v1",
-        hide_documentation_path: true,
-        mount_path: "/api/v1/swagger_doc"
-      )
     end
   end
 end
